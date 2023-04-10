e2a4648e2cebd8f3dce5de8f9463efe9a26737fa
TWloYWkgQm9kbmFyaXU= <mihai.bodnariu@butonium.com>
2023-04-10 20:38:44 +0300

    fix: relative path

--- scrapper/scrapper.sh
+++ scrapper/scrapper.sh
@@ -8,5 +8,6 @@ python3 -m venv venv && \
 source ./venv/bin/activate && \
 pip3 install -r requirements.txt
 python3 snapshot-finder.py --max_latency 400 --min_download_speed 1 --snapshot_path /data/
+cd /data
 /usr/local/bin/solana-snapshot-etl "$(ls *.tar.zst | tail -n 1)" --sqlite-out /data/snapshot.db
 exec "$@"
