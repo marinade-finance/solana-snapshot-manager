e3f87099de11b9f06a2881382a7aa8df85dd5787
TWloYWkgQm9kbmFyaXU= <mihai.bodnariu@butonium.com>
2023-04-12 18:04:59 +0300

    fix: add docker login

--- scraper/run-scraper.sh
+++ scraper/run-scraper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -ex
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 400405091548.dkr.ecr.eu-central-1.amazonaws.com
 docker compose rm -f -s -v
 docker compose up -d
