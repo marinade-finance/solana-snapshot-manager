2ee23f9254473d4b50d730547f4556b0f9c7b092
chalda <chalda@marinade.finance>
2024-03-19 16:44:02 +0100

    [list-stakers] cli command to work faster, more logging info (#59)
    * [list-stakers] cli command to work faster, more logging info
    
    * [list-stakers] a small log print addition
    
    * Update src/cli/list-stakers.cmd.ts
    
    Co-authored-by: martinkrecek <9853061+martinkrecek@users.noreply.github.com>
    
    * adding warning in case of date parameters were not provided
    
    ---------
    
    Co-authored-by: martinkrecek <9853061+martinkrecek@users.noreply.github.com>

--- src/cli/list-stakers.cmd.ts
+++ src/cli/list-stakers.cmd.ts
@@ -2,7 +2,7 @@ import { Command, CommandRunner, Option } from 'nest-commander';
 import { Logger } from '@nestjs/common';
 import { StakersService } from 'src/stakers/stakers.service';
 import fs from 'fs';
-import json from 'big-json';
+import { JsonStreamStringify } from 'json-stream-stringify';
 
 type ListStakersCommandOptions = {
   startDate: string;
@@ -22,7 +22,7 @@ export class ListStakersCommand extends CommandRunner {
     flags: '--start-date <string>',
     description:
       'Start date (e.g., 2023-01-01) from snapshot taken time for fetching stakers data. ' +
-      'When not defined current date is taken. ' +
+      'When not defined <current date> is taken. ' +
       'The start date is inclusive, the date is calculated into range.',
   })
   parseArgStartDate(val: string): string {
@@ -33,7 +33,7 @@ export class ListStakersCommand extends CommandRunner {
     flags: '--end-date <string>',
     description:
       'End date (e.g., 2023-01-18) till the snapshots taken time for fetching stakers data. ' +
-      'When not defined current date is taken. ' +
+      'When not defined <current date + 1> is taken. ' +
       'The end date is exclusive, the date is not calculated into range.',
   })
   parseArgEndDate(val: string): string {
@@ -57,31 +57,63 @@ export class ListStakersCommand extends CommandRunner {
     passedParam: string[],
     { startDate, endDate, output }: ListStakersCommandOptions,
   ): Promise<void> {
+    const logger = this.logger;
     if (!output) {
       throw new Error('argument --output is required');
     }
 
-    const logger = this.logger;
+    if (!startDate) {
+      const currentDay = new Date().toISOString().split('T')[0];
+      if (currentDay === undefined) {
+        throw new Error(
+          'Cannot get the current date, use --start-date parameter please',
+        );
+      }
+      logger.warn('☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️');
+      logger.warn(
+        `⚠️⚠️⚠️ --start-date param not provided, using current date: '${currentDay}' ⚠️⚠️⚠️`,
+      );
+      logger.warn('☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️');
+      startDate = currentDay;
+    }
+    if (!endDate) {
+      const tomorrowDay = new Date();
+      tomorrowDay.setDate(tomorrowDay.getDate() + 1);
+      const tomorrowDayStr = tomorrowDay.toISOString().split('T')[0];
+      if (tomorrowDayStr === undefined) {
+        throw new Error(
+          'Cannot get the tomorrow date, use --end-date parameter please',
+        );
+      }
+      logger.warn('☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️');
+      logger.warn(
+        `⚠️⚠️⚠️ --end-date param not provided, using tomorrow date: '${tomorrowDayStr}' ⚠️⚠️⚠️`,
+      );
+      logger.warn('☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️☠️');
+      endDate = tomorrowDayStr;
+    }
+
     logger.log(
-      `Listing all stakers (liquid and native) in period [${
-        startDate || 'now'
-      } - ${endDate || 'now'})`,
+      `Loading DB all stakers (liquid and native) in period [${startDate} (inclusive) - ${endDate} (exclusive)]`,
     );
+    const dbLoadStartTime = Date.now();
     const allStakers = await this.stakersService.getAllStakersBalances(
       startDate,
       endDate,
     );
+    logger.log(
+      `DB loaded in '${(Date.now() - dbLoadStartTime) / 1000} seconds'`,
+    );
 
     logger.log(`Saving data as JSON to '${output}'`);
-    const startTime = new Date();
+    const jsonWriteStartTime = Date.now();
     const fileWriteStream = fs.createWriteStream(output);
-    json
-      .createStringifyStream({ body: allStakers })
+    new JsonStreamStringify(allStakers)
       .pipe(fileWriteStream)
       .on('finish', () =>
         logger.log(
-          `Data written in '${
-            (new Date().getTime() - startTime.getTime()) / 1000
+          `Data written into ${output} in '${
+            (Date.now() - jsonWriteStartTime) / 1000
           } seconds'`,
         ),
       );
