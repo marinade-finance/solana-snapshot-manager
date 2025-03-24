04e02f83982a2ccb9a488217963d93587705c9d2
chalda <chalda@marinade.finance>
2025-03-24 11:09:16 +0100

    [cli] record msol votes in separate step + fail in cli exception (#92)
    * [cli] record msol votes in separate step + fail in cli exception
    
    * [buildkite] removal of missing property
    
    * fixes

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,17 @@ import { CliModule } from './cli/cli.module';
 import { Logger } from './logger';
 
 async function bootstrap() {
-  await CommandFactory.run(CliModule, new Logger());
+  await CommandFactory.run(CliModule, {
+    logger: new Logger(),
+    errorHandler: (err) => {
+      console.error('Error handler:', err);
+      process.exit(1);
+    },
+    serviceErrorHandler: (err) => {
+      console.error('Service error:', err);
+      process.exit(2);
+    },
+  });
 }
 
 bootstrap();
